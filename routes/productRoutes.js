61f838cebabd6f2ab3952eb74312870c933d0c92
Du-Previatti <dudupreviatti@gmail.com>
2023-07-20 19:59:31 -0300

    GetProduct Fix 1.0

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,24 +8,24 @@ router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProduct);
 
 router.post('/', authMiddleware, (req, res, next) => {
-    if (!req.isAdmin) {
-        return res.status(403).send({ error: 'User does not have permission to perform this action' });
-    }
-    next();
+  if (!req.isAdmin) {
+    return res.status(403).send({ error: 'User does not have permission to perform this action' });
+  }
+  next();
 }, productController.createProduct);
 
 router.put('/:id', authMiddleware, (req, res, next) => {
-    if (!req.isAdmin) {
-        return res.status(403).send({ error: 'User does not have permission to perform this action' });
-    }
-    next();
+  if (!req.isAdmin) {
+    return res.status(403).send({ error: 'User does not have permission to perform this action' });
+  }
+  next();
 }, productController.updateProduct);
 
 router.delete('/:id', authMiddleware, (req, res, next) => {
-    if (!req.isAdmin) {
-        return res.status(403).send({ error: 'User does not have permission to perform this action' });
-    }
-    next();
+  if (!req.isAdmin) {
+    return res.status(403).send({ error: 'User does not have permission to perform this action' });
+  }
+  next();
 }, productController.deleteProduct);
 
 module.exports = router;
