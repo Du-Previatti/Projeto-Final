4ef4656ea7d710f1e7dd465e4db51d33ceaa98e9
Du-Previatti <dudupreviatti@gmail.com>
2023-07-24 19:04:27 -0300

    Projeto final - Front

---
+++ Front/src/App.tsx
@@ -0,0 +1,20 @@
+import { CartProvider } from "./Context/CartContext"
+import Router from "./routes/index.router"
+
+
+function App() {
+  
+  return (
+  
+  <CartProvider>
+    <Router/>
+   </CartProvider>
+   
+  
+  )
+}
+      
+
+
+
+export default App
