cf767052f683e7fbb98c6b2421f8b9f60e6a36d6
Du-Previatti <dudupreviatti@gmail.com>
2023-07-24 20:42:51 -0300

    Add Front fix 2.0

---
+++ Front/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "my-vite-app",
+        script: "npm",
+        args: "run dev",
+        env: {
+          NODE_ENV: "development",
+        },
+      },
+    ],
+  };
+  
